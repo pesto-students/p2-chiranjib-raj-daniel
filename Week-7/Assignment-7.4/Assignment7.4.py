d7a85c7cd082e9d5a2cb70e29f4a8d7708c57366
daniel-raj <dani85rj@gmail.com>
2022-06-25 00:34:48 +0530

    resubmitting assignment 4 with changes and comments

--- Week-7/Assignment-7.4/Assignment7.4.py
+++ Week-7/Assignment-7.4/Assignment7.4.py
@@ -1,19 +1,49 @@
 
+# def parenthesisChecker(arr):
+#     charStack = []
+#     openTags = ["(","[","{"]
+#     closeTags = [")","]","}"]
+
+#     for value in range(len(arr)):
+#         if arr[value]  in openTags:
+#             print(value)
+#             charStack.append(arr[value])
+#         elif arr[value]  in closeTags:
+#             print(value)
+#             checkTag=charStack.pop()
+#             if ord(checkTag) != ord(arr[value]) - 1 or  ord(arr[value]) - 2:
+#                 return False
+#             elif ord(checkTag) != ord(arr[value]) - 2:
+#                 return False
+#     return True 
+
+# print(parenthesisChecker(list("{}[]()")))
+
+
+
 def parenthesisChecker(arr):
-    charStack = []
-    openTags = ["(","[","{"]
-    closeTags = [")","]","}"]
+    charStack = []                   #empty arrray to store the opening characters
+    openTags = ["(","[","{"]         # opening comparison characters
+    closeTags = [")","]","}"]        # closing comparison characters
 
     for value in range(len(arr)):
         if arr[value]  in openTags:
-            print(value)
-            charStack.append(arr[value])
+            charStack.append(arr[value])            # appending to charstack if iterator value belongs to opening tag
         elif arr[value]  in closeTags:
-            print(value)
-            if charStack.pop() != value:
-                return False
-            else:
+            checkTag=charStack.pop()                # poping and storing the opening tag into temp variable
+            if arr[value] == "]" and checkTag == "[":       # comparing opening and closing tag belongs to same group or not, passing if the same if not returning false
                 pass
+            elif arr[value] == ")" and checkTag == "(":
+                pass
+            elif arr[value] == "}" and checkTag == "{":
+                pass
+            else:
+                return False
     return True 
 
-print(parenthesisChecker(list("{}[]()")))
+print(parenthesisChecker(list("{[}]()"))) .  # passing the string into list(array)
+
+
+
+# time complexity is O(n)
+# space complexity is O(n)
