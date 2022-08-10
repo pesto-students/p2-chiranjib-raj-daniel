43566e2c2f6fd67c517abbc71c17f4915a4416a3
daniel-raj <dani85rj@gmail.com>
2022-08-10 21:34:30 +0530

    added data and search queries2

--- Week-12/queries.sql
+++ Week-12/queries.sql
@@ -1,13 +1,13 @@
-SELECT * FROM items WHERE weight = (SELECT MIN(weight) FROM items);
+SELECT * FROM ITEMS WHERE weight = (SELECT MIN(weight) FROM ITEMS);
 
-SELECT * FROM warehouses WHERE location = "Pune";
+SELECT * FROM WAREHOUSES WHERE location = "Pune";
 
-SELECT * FROM orders
-INNER JOIN customer ON orders.customer_id = customer.cno
-INNER JOIN items ON orders.item_id = items.itemno;
+SELECT * FROM ORDERS
+INNER JOIN CUSTOMER ON ORDERS.customer_id = customer.cno
+INNER JOIN ITEMS ON ORDERS.item_id = ITEMS.itemno;
 
-SELECT * FROM items  WHERE itemno = (SELECT item_id FROM orders GROUP BY item_id ORDER BY count(item_id) limit 1);
+SELECT * FROM ITEMS  WHERE itemno = (SELECT ONO FROM ORDERS GROUP BY ONO ORDER BY count(ONO) limit 1);
 
-SELECT * FROM stores WHERE warehouses_id = (SELECT warehouses_id FROM GROUP BY warehouses_id ORDER BY count(warehouses_id) desc limit 1);
+SELECT * FROM STORES WHERE WID = (SELECT WID FROM GROUP BY WID ORDER BY count(WID) desc limit 1);
 
-SELECT * FROM orders INNER JOIN customer ON orders.customer_id = customer.cno WHERE cname = "Mr. Patil";
+SELECT * FROM ORDERS INNER JOIN CUSTOMER ON ORDERS.ONO = customer.cno WHERE cname = "Mr. Patil";
